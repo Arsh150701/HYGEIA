7aed1816d7f4ee52f55263fd233911dff1bc20e2
Devansh <201851038@iiitvadodara.ac.in>
2021-01-23 01:54:54 +0530

    Modified clinic add activity

--- app/src/main/java/com/example/hygeia/DoctorClinicsInitActivity.java
+++ app/src/main/java/com/example/hygeia/DoctorClinicsInitActivity.java
@@ -1,7 +1,9 @@
 package com.example.hygeia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,182 +13,135 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 
+//isko fragment banana hai
 public class DoctorClinicsInitActivity extends AppCompatActivity {
 
     private static final String TAG = "DoctorClinicsInitActivi";
 
     private CheckBox mon, tue, wed, thur, fri, sat, sun;
-    private TextInputLayout doclinicbox, monbox, tuebox, wedbox, thurbox, fribox, satbox, sunbox,monbox1, tuebox1, wedbox1, thurbox1, fribox1, satbox1, sunbox1;
-    private TextInputEditText doclinic, montext, tuetext, wedtext, thurtext, fritext, sattext, suntext,montext1, tuetext1, wedtext1, thurtext1, fritext1, sattext1, suntext1;
     private Button addclinic;
+    private Button btnstmon, btnsttue, btnstwed, btnstthur, btnstfri, btnstsat, btnstsun;
+    private Button btnetmon, btnettue, btnetwed, btnetthur, btnetfri, btnetsat, btnetsun;
+    private TextInputLayout doclinicbox;
+    private TextInputEditText doclinic;
+
+    private List<String> array_title = new ArrayList<>();
+    int t1, t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_clinics_init);
 
-        doclinicbox=findViewById(R.id.docclinicbox);
-        doclinic=findViewById(R.id.docclinic);
-        addclinic=findViewById(R.id.addclinic);
+        doclinicbox = findViewById(R.id.docclinicbox);
+        doclinic = findViewById(R.id.docclinic);
+        addclinic = findViewById(R.id.addclinic);
 
 
-        mon=findViewById(R.id.checkmon);
-        montext=findViewById(R.id.sttimemon);
-        monbox=findViewById(R.id.sttimeboxmon);
-        montext1=findViewById(R.id.ettimemon);
-        monbox1=findViewById(R.id.ettimeboxmon);
+        mon = findViewById(R.id.checkmon);
+        btnstmon = findViewById(R.id.sttimemon);
+        btnetmon = findViewById(R.id.ettimemon);
 
-        tue=findViewById(R.id.checktue);
-        tuetext=findViewById(R.id.sttimetue);
-        tuebox=findViewById(R.id.sttimeboxtue);
-        tuetext1=findViewById(R.id.ettimetue);
-        tuebox1=findViewById(R.id.ettimeboxtue);
+        tue = findViewById(R.id.checktue);
+        btnsttue = findViewById(R.id.sttimetue);
+        btnettue = findViewById(R.id.ettimetue);
 
-        wed=findViewById(R.id.checkwed);
-        wedtext=findViewById(R.id.sttimewed);
-        wedbox=findViewById(R.id.sttimeboxwed);
-        wedtext1=findViewById(R.id.ettimewed);
-        wedbox1=findViewById(R.id.ettimeboxwed);
+        wed = findViewById(R.id.checkwed);
+        btnstwed = findViewById(R.id.sttimewed);
+        btnetwed = findViewById(R.id.ettimewed);
 
-        thur=findViewById(R.id.checkthur);
-        thurtext=findViewById(R.id.sttimethur);
-        thurbox=findViewById(R.id.sttimeboxthur);
-        thurtext1=findViewById(R.id.ettimethur);
-        thurbox1=findViewById(R.id.ettimeboxthur);
+        thur = findViewById(R.id.checkthur);
+        btnstthur = findViewById(R.id.sttimethur);
+        btnetthur = findViewById(R.id.ettimethur);
 
-        fri=findViewById(R.id.checkfri);
-        fritext=findViewById(R.id.sttimefri);
-        fribox=findViewById(R.id.sttimeboxfri);
-        fritext1=findViewById(R.id.ettimefri);
-        fribox1=findViewById(R.id.ettimeboxfri);
+        fri = findViewById(R.id.checkfri);
+        btnstfri = findViewById(R.id.sttimefri);
+        btnetfri = findViewById(R.id.ettimefri);
 
-        sat=findViewById(R.id.checksat);
-        sattext=findViewById(R.id.sttimesat);
-        satbox=findViewById(R.id.sttimeboxsat);
-        sattext1=findViewById(R.id.ettimesat);
-        satbox1=findViewById(R.id.ettimeboxsat);
+        sat = findViewById(R.id.checksat);
+        btnstsat = findViewById(R.id.sttimesat);
+        btnetsat = findViewById(R.id.ettimesat);
 
-        sun=findViewById(R.id.checksun);
-        suntext=findViewById(R.id.sttimesun);
-        sunbox=findViewById(R.id.sttimeboxsun);
-        suntext1=findViewById(R.id.ettimesun);
-        sunbox1=findViewById(R.id.ettimeboxsun);
-
+        sun = findViewById(R.id.checksun);
+        btnstsun = findViewById(R.id.sttimesun);
+        btnetsun = findViewById(R.id.ettimesun);
 
         setWatcher(doclinicbox, doclinic);
+        checkchange(mon, btnstmon, btnetmon);
+        checkchange(tue, btnsttue, btnettue);
+        checkchange(wed, btnstwed, btnetwed);
+        checkchange(thur, btnstthur, btnetthur);
+        checkchange(fri, btnstfri, btnetfri);
+        checkchange(sat, btnstsat, btnetsat);
+        checkchange(sun, btnstsun, btnetsun);
 
-        mon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    monbox.setVisibility(View.VISIBLE);
-                    monbox1.setVisibility(View.VISIBLE);
-                }else {
-                    monbox.setVisibility(View.GONE);
-                    monbox1.setVisibility(View.GONE);
-                }
-            }
-        });
-        tue.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    tuebox.setVisibility(View.VISIBLE);
-                    tuebox1.setVisibility(View.VISIBLE);
-                }else {
-                    tuebox.setVisibility(View.GONE);
-                    tuebox1.setVisibility(View.GONE);
-                }
-            }
-        });
-        wed.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    wedbox.setVisibility(View.VISIBLE);
-                    wedbox1.setVisibility(View.VISIBLE);
-                }else {
-                    wedbox.setVisibility(View.GONE);
-                    wedbox1.setVisibility(View.GONE);
-                }
-            }
-        });
-        thur.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    thurbox.setVisibility(View.VISIBLE);
-                    thurbox1.setVisibility(View.VISIBLE);
-                }else {
-                    thurbox.setVisibility(View.GONE);
-                    thurbox1.setVisibility(View.GONE);
-                }
-            }
-        });
-        fri.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    fribox.setVisibility(View.VISIBLE);
-                    fribox1.setVisibility(View.VISIBLE);
-                }else {
-                    fribox.setVisibility(View.GONE);
-                    fribox1.setVisibility(View.GONE);
-                }
-            }
-        });
-        sat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    satbox.setVisibility(View.VISIBLE);
-                    satbox1.setVisibility(View.VISIBLE);
-                }else {
-                    satbox.setVisibility(View.GONE);
-                    satbox1.setVisibility(View.GONE);
-                }
-            }
-        });
-        sun.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    sunbox.setVisibility(View.VISIBLE);
-                    sunbox1.setVisibility(View.VISIBLE);
-                }else {
-                    sunbox.setVisibility(View.GONE);
-                    sunbox1.setVisibility(View.GONE);
-                }
-            }
-        });
+        timepicker(btnstmon);
+        timepicker(btnetmon);
+        timepicker(btnsttue);
+        timepicker(btnettue);
+        timepicker(btnstwed);
+        timepicker(btnetwed);
+        timepicker(btnstthur);
+        timepicker(btnetthur);
+        timepicker(btnstfri);
+        timepicker(btnetfri);
+        timepicker(btnstsat);
+        timepicker(btnetsat);
+        timepicker(btnstsun);
+        timepicker(btnetsun);
+
 
         addclinic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mon.isChecked())
-                    saveDetails(montext, montext1, mon);
-                if(tue.isChecked())
-                    saveDetails(tuetext, tuetext1, tue);
-                if(wed.isChecked())
-                    saveDetails(wedtext, wedtext1, wed);
-                if(thur.isChecked())
-                    saveDetails(thurtext, thurtext1, thur);
-                if(fri.isChecked())
-                    saveDetails(fritext, fritext1, fri);
-                if(sat.isChecked())
-                    saveDetails(sattext, sattext1, sat);
-                if(sun.isChecked())
-                    saveDetails(suntext, suntext1, sun);
+                if (mon.isChecked())
+                    saveDetails(btnstmon, btnetmon, mon);
+                if (tue.isChecked())
+                    saveDetails(btnsttue, btnettue, tue);
+                if (wed.isChecked())
+                    saveDetails(btnstwed, btnetwed, wed);
+                if (thur.isChecked())
+                    saveDetails(btnstthur, btnetthur, thur);
+                if (fri.isChecked())
+                    saveDetails(btnstfri, btnetfri, fri);
+                if (sat.isChecked())
+                    saveDetails(btnstsat, btnetsat, sat);
+                if (sun.isChecked())
+                    saveDetails(btnstsun, btnetsun, sun);
                 Log.d(TAG, "onClick: buttonclick");
+            }
+        });
+    }
+
+    public void checkchange(CheckBox checkBox, Button btn1, Button btn2) {
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    btn1.setVisibility(View.VISIBLE);
+                    btn2.setVisibility(View.VISIBLE);
+                } else {
+                    btn1.setVisibility(View.GONE);
+                    btn2.setVisibility(View.GONE);
+                }
             }
         });
     }
@@ -195,7 +150,6 @@ public class DoctorClinicsInitActivity extends AppCompatActivity {
         TextWatcher watcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
@@ -205,25 +159,92 @@ public class DoctorClinicsInitActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-
             }
         };
         editText.addTextChangedListener(watcher);
     }
 
-    private void saveDetails(TextInputEditText textInputEditText, TextInputEditText textInputEditText1, CheckBox checkBox){
-
-        Log.d(TAG, "saveDetails: inside save details");
+    private void saveDetails(Button btn1, Button btn2, CheckBox checkBox) {
+        Log.d(TAG, "saveDetails1: save");
         HashMap<String, Object> map = new HashMap<>();
+        map.put("startTime", btn1.getText().toString());
+        map.put("endTime", btn2.getText().toString());
 
-        map.put("startTime", textInputEditText.getText().toString());
-        map.put("endTime", textInputEditText1.getText().toString());
-
-        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("doctors")
                 .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
                 .collection(doclinic.getText().toString())
                 .document(checkBox.getText().toString())
                 .set(map, SetOptions.merge());
+
+        map.clear();
+        db.collection("doctors")
+                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot doc = task.getResult();
+                            assert doc != null;
+                            String clinics = doc.getString("Visits");
+                            try {
+                                array_title = Arrays.asList(clinics.split(","));
+                                int exist = 0;
+
+                                for (int i = 0; i < array_title.size(); ++i) {
+                                    String clinicName = array_title.get(i);
+                                    if (clinicName.equals(doclinic.getText().toString())) {
+                                        Log.d(TAG, "onComplete: name exists");
+                                        Toast.makeText(DoctorClinicsInitActivity.this, "Already exists", Toast.LENGTH_SHORT).show();
+                                        exist = 1;
+                                    }
+                                }
+                                if (exist == 0) {
+                                    map.clear();
+                                    Log.d(TAG, "onComplete: name doest not exist");
+                                    map.put("Visits", clinics + "," + doclinic.getText().toString());
+                                    db.collection("doctors")
+                                            .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .set(map, SetOptions.merge());
+                                }
+                            } catch (Exception e) {
+                                Log.d(TAG, "onComplete: " + e.getMessage());
+                                Log.d(TAG, "onComplete: field does not exist");
+                                map.clear();
+                                map.put("Visits", doclinic.getText().toString());
+                                db.collection("doctors")
+                                        .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .set(map, SetOptions.merge());
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void timepicker(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        DoctorClinicsInitActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                t1 = i;
+                                t2 = i1;
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.set(0, 0, 0, i, i1);
+                                if (i1 == 0)
+                                    btn.setText(new StringBuilder().append(i).append(":").append("00"));
+                                else
+                                    btn.setText(new StringBuilder().append(i).append(":").append(i1));
+                            }
+                        }, 12, 0, false
+                );
+                timePickerDialog.updateTime(t1, t2);
+                timePickerDialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hygeia/Splash.java
+++ app/src/main/java/com/example/hygeia/Splash.java
@@ -1,23 +1,48 @@
 package com.example.hygeia;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Splash extends AppCompatActivity {
+    private static final String TAG = "Splash";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null)
             startActivity(new Intent(this, Intro_Signin.class));
-        else
-            startActivity(new Intent(this, MainActivity.class));//yaha change karna hai
+        else {
+            /*FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Log.d(TAG, "onCreate: " + FirebaseAuth.getInstance().getCurrentUser().getUid());
+            Log.d(TAG, "onCreate: " + user);
+
+            db.collection("users")
+                    .document(user.getUid())
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful())
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            else
+                                startActivity(new Intent(getApplicationContext(), DoctorClinicsInitActivity.class));
+                        }
+                    });*/
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
         onBackPressed();
     }
 
