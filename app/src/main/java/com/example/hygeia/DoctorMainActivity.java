cfe0947abed0390e441e65408a313ec648e6b33a
Devansh <201851038@iiitvadodara.ac.in>
2021-01-29 01:45:35 +0530

    Added a drawer for doctor.

--- app/src/main/java/com/example/hygeia/DoctorMainActivity.java
+++ app/src/main/java/com/example/hygeia/DoctorMainActivity.java
@@ -1,14 +1,58 @@
 package com.example.hygeia;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DoctorMainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DoctorMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_main);
+
+        toolbar=findViewById(R.id.doctoolbar);
+        setSupportActionBar(toolbar);
+
+        drawer=findViewById(R.id.docdrawerlayout);
+        navigationView=findViewById(R.id.docnavigation);
+
+        getDrawerStarted();
+
+    }
+
+    private void getDrawerStarted(){
+        ActionBarDrawerToggle drawerToggle=new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+
+        drawer.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
